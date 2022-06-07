77b5ca358ffd3febe31404d9ccddcc5f81e9a244
Darlan <darlan.silvadacruz@gmail.com>
2022-06-07 10:34:52 -0300

    :sparkles:(_app.tsx) add global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 
 import AuthProvider from "@contexts/AuthContext";
+import GlobalStyle from "globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <GlobalStyle />
       <Component {...pageProps} />
     </AuthProvider>
   );
