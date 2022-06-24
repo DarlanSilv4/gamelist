0cf3469d6cbcb20cb56c7cc530726dbdd0c6e165
Darlan <darlan.silvadacruz@gmail.com>
2022-06-24 11:48:49 -0300

    :sparkles: set up login page

---
+++ pages/login.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+import { useRouter } from "next/router";
+
+import { useAuth } from "@contexts/AuthContext";
+import Login from "@templates/Login";
+
+function LoginPage() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user]);
+
+  return <Login />;
+}
+
+export default LoginPage;
