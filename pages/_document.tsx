57d1b7e910c73a0df3c7229dba93eff74b909738
Darlan <darlan.silvadacruz@gmail.com>
2022-07-16 11:37:03 -0300

    :sparkles:(_document.tsx) add stylesheet for fonts and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 
 import { ServerStyleSheet } from "styled-components";
 
@@ -26,5 +33,31 @@ export default class MyDocument extends Document {
         sheet.seal();
       }
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="favicon.svg" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
