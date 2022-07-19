3cccd0e63fff43b0cb5dc48aafb51c278d93e727
Darlan <darlan.silvadacruz@gmail.com>
2022-07-19 13:24:02 -0300

    :recycle:(AuthContext) remove signInWithGoogle function

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -1,17 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-import {
-  getRedirectResult,
-  GoogleAuthProvider,
-  signInWithRedirect,
-  User as FirebaseUser,
-} from "firebase/auth";
-
 import { auth } from "@lib/firebaseConfig";
 
 type AuthContextType = {
   user: User | null;
-  signInWithGoogle: () => Promise<void>;
   signOut: () => Promise<void>;
 };
 
@@ -38,30 +30,12 @@ function AuthProvider(props: React.PropsWithChildren<{}>) {
     };
   }, []);
 
-  const signInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-
-    await signInWithRedirect(auth, provider);
-
-    const result = await getRedirectResult(auth);
-
-    if (result?.user) {
-      const { displayName, photoURL, uid } = result.user;
-
-      setUser({
-        id: uid,
-        name: displayName || `user - ${uid}`,
-        avatar: photoURL,
-      });
-    }
-  };
-
   const signOut = async () => {
     auth.signOut();
   };
 
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle, signOut }}>
+    <AuthContext.Provider value={{ user, signOut }}>
       {props.children}
     </AuthContext.Provider>
   );
