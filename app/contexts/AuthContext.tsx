1b3cab9a6b5af0871180fd3dba6c7a6b4eb271a8
Darlan <darlan.silvadacruz@gmail.com>
2022-07-19 13:25:23 -0300

    :recycle:(AuthContext) get auth from firebase folder

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { auth } from "@lib/firebaseConfig";
+import { auth } from "@firebase/firebaseConfig";
 
 type AuthContextType = {
   user: User | null;
