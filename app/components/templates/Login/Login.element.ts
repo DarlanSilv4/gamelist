5b9d77cc378a5c57a2150e1fdbdeae265d395c93
Darlan <darlan.silvadacruz@gmail.com>
2022-07-21 10:10:33 -0300

    :sparkles:(templates/login) show loading spinner while waiting for authentication

--- app/components/templates/Login/Login.element.ts
+++ app/components/templates/Login/Login.element.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 export const Wrapper = styled.div`
   align-items: center;
@@ -7,4 +7,32 @@ export const Wrapper = styled.div`
   height: 80vh;
   justify-content: center;
   width: 100%;
+`;
+
+const Rotation = keyframes`
+ 0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+  display: inline-block;
+  height: 80px;
+  width: 80px;
+
+  &:after {
+    animation: ${Rotation} 1.2s linear infinite;
+    border: 6px solid hsl(100, 100%, 90%);
+    border-color: hsl(100, 100%, 90%) transparent hsl(100, 100%, 90%)
+      transparent;
+    border-radius: 50%;
+    content: " ";
+    display: block;
+    height: 64px;
+    margin: 8px;
+    width: 64px;
+  }
 `;
--- app/components/templates/Login/index.tsx
+++ app/components/templates/Login/index.tsx
@@ -1,17 +1,27 @@
+import React from "react";
+
 import Image from "next/image";
 
+import Head from "@elements/Head";
 import GoogleButton from "@elements/GoogleButton";
 
-import { Wrapper } from "./Login.element";
+import { useAuth } from "@contexts/AuthContext";
+
+import { Loading, Wrapper } from "./Login.element";
 
 function Login() {
+  const [user, isLoading] = useAuth();
+
   return (
-    <Wrapper>
-      <figure>
-        <Image src="/svg/logo.svg" width={160} height={160} />
-      </figure>
-      <GoogleButton />
-    </Wrapper>
+    <React.Fragment>
+      <Head title="Login | Gamelist" />
+      <Wrapper>
+        <figure>
+          <Image src="/svg/logo.svg" width={160} height={160} />
+        </figure>
+        {isLoading || user ? <Loading /> : <GoogleButton />}
+      </Wrapper>
+    </React.Fragment>
   );
 }
 
