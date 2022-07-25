a7ac5788090f0c9cdf6308fde1999457454916ef
Darlan <darlan.silvadacruz@gmail.com>
2022-07-25 12:24:09 -0300

    :sparkles: set up search bar

--- app/components/elements/SearchBar/index.tsx
+++ app/components/elements/SearchBar/index.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+
+import { useRouter } from "next/router";
+
 import { InputBar, SearchContainer, SearchIcon } from "./SearchBar.element";
 
-function SearchBar() {
+function SearchBar({ value = "" }: { value?: string }) {
+  const router = useRouter();
+
+  const [searchText, setSearchText] = useState(value);
+  const [searchDebounce, setSearchDebounce] = useState<NodeJS.Timeout>();
+
+  const isTextEmpty = (text: string) => !Boolean(text);
+
+  const handlerSearch = (title: string) => {
+    setSearchText(title);
+
+    if (isTextEmpty(title) && router.asPath !== "/") {
+      clearTimeout(searchDebounce);
+      return router.push("/");
+    }
+
+    const debounce = setTimeout(() => {
+      router.push(`/search/${title}`);
+    }, 300);
+
+    clearTimeout(searchDebounce);
+    return setSearchDebounce(debounce);
+  };
+
   return (
     <SearchContainer>
-      <InputBar type="text" placeholder="Search game titles..." />
+      <InputBar
+        type="text"
+        placeholder="Search game titles..."
+        value={searchText}
+        onChange={({ target }) => handlerSearch(target.value)}
+      />
       <SearchIcon className="material-icons-round">search</SearchIcon>
     </SearchContainer>
   );
