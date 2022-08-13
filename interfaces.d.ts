0aa458e07333552a30f2c0beee7a81dfc54b734a
Darlan <darlan.silvadacruz@gmail.com>
2022-08-13 11:27:59 -0300

    :sparkles:(interfaces.d.ts) create an interface and a type for listed game and game state

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -10,6 +10,16 @@ interface Game {
   platforms: Platform[];
 }
 
+type GameState = "playing" | "played" | "dropped" | "wishlist";
+
+// Since the user list of games and the games themselves are from different APIs,
+// it was necessary to create a new interface only for the games that are in the
+// user list.
+interface ListedGame {
+  game_id: string;
+  state: GameState;
+}
+
 interface Platform {
   id: number;
   image_id: string;
