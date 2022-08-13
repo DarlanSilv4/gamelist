78a0f28a755d17557c657ef2ef30f383f9ed0713
Darlan <darlan.silvadacruz@gmail.com>
2022-08-13 13:32:11 -0300

    :sparkles:(GameCard/index.tsx) set up add to list button and state button

--- app/components/elements/GameCard/index.tsx
+++ app/components/elements/GameCard/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
+import { useAuth } from "@contexts/AuthContext";
+
 import getCoverUrl from "@lib/getCoverUrl";
+import { writeGameInTheList } from "@lib/gamelist";
+
+import useClickOutside from "hooks/useClickOutside";
 
 import {
   CardContainer,
@@ -10,6 +17,9 @@ import {
   GameTitle,
   Platform,
   AddButton,
+  StateButton,
+  DropdownOptions,
+  Dropdown,
 } from "./GameCard.element";
 
 interface GameCardProps {
@@ -17,6 +27,23 @@ interface GameCardProps {
 }
 
 function GameCard({ game }: GameCardProps) {
+  const DROPDOWN_OPTIONS: Array<GameState> = [
+    "playing",
+    "played",
+    "dropped",
+    "wishlist",
+  ];
+
+  const [user] = useAuth();
+
+  const [gameState, setGameState] = useState<GameState>();
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const dropdownNode = useClickOutside<HTMLUListElement>(() =>
+    setIsDropdownOpen(false)
+  );
+
   const coverUrl = getCoverUrl(game.cover);
 
   const getFormattedPlatforms = (platforms: Platform[]) => {
@@ -31,8 +58,28 @@ function GameCard({ game }: GameCardProps) {
     return "";
   };
 
+  const handleAddToList = async (state: GameState) => {
+    if (!user) return;
+
+    await writeGameInTheList({ game_id: game.id, state: state }, user.id);
+  };
+
+  useEffect(() => {
+    const getGameState = () => {
+      if (!user || !user.gamelist) return;
+
+      const listedGame = user.gamelist.find(
+        (listedGame) => listedGame.game_id == game.id
+      );
+
+      if (listedGame) setGameState(listedGame.state);
+    };
+
+    getGameState();
+  }, [user]);
+
   return (
-    <CardContainer>
+    <CardContainer state={gameState}>
       <Link href={`game/${game.id}`} passHref>
         <CoverWrapper>
           <Image src={coverUrl} layout="fill" />
@@ -44,7 +91,32 @@ function GameCard({ game }: GameCardProps) {
           <GameTitle>{game.name}</GameTitle>
         </Link>
         <Platform>{getFormattedPlatforms(game.platforms)}</Platform>
-        <AddButton>+ Add to List</AddButton>
+
+        {gameState ? (
+          <StateButton onClick={() => setIsDropdownOpen(true)}>
+            <span>{gameState}</span>
+            <span className="material-icons-round">arrow_drop_down</span>
+          </StateButton>
+        ) : (
+          <AddButton onClick={async () => await handleAddToList("playing")}>
+            + Add to List
+          </AddButton>
+        )}
+
+        <Dropdown ref={dropdownNode} isOpen={isDropdownOpen}>
+          {DROPDOWN_OPTIONS.map((state) => {
+            return (
+              <DropdownOptions
+                onClick={() => {
+                  handleAddToList(state);
+                  setIsDropdownOpen(false);
+                }}
+              >
+                {state}
+              </DropdownOptions>
+            );
+          })}
+        </Dropdown>
       </Info>
     </CardContainer>
   );
