f552002a1ea988bef5c0f3dcda79fc9800a3b3d1
Darlan <darlan.silvadacruz@gmail.com>
2022-09-07 12:17:22 -0300

    :bug:(_document.tsx) fix favicon just showing on homepage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="shortcut icon" href="favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.svg" />
 
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
