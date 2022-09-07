a1dcd69418b8c0e6e05cf6f416fe38fb17be6ecf
Darlan <darlan.silvadacruz@gmail.com>
2022-09-07 11:59:48 -0300

    :recycle: (elements/Profile/index.tsx) change links that go to the user profile page

--- app/components/elements/Profile/index.tsx
+++ app/components/elements/Profile/index.tsx
@@ -26,7 +26,7 @@ function Profile() {
 
   return (
     <ProfileContainer>
-      <Link href="profile/" passHref>
+      <Link href={`/user/${user?.id}`} passHref>
         <Avatar>
           <Image src={user?.avatar || "/me.png"} layout="fill" />
         </Avatar>
@@ -45,7 +45,7 @@ function Profile() {
         </DropdownButton>
 
         <DropdownOptions isOpen={isOpen}>
-          <Link href="profile/" passHref>
+          <Link href={`/user/${user?.id}`} passHref>
             <DropdownItem>My Profile</DropdownItem>
           </Link>
           <Link href="/" passHref>
