b7b24743f3bb1388b8f73a4905e9d6168a6e8d36
Darlan <darlan.silvadacruz@gmail.com>
2022-09-08 11:30:05 -0300

    :bug:(next.config.js) fix material icon fonts break in production

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.igdb.com', 'lh3.googleusercontent.com']
-  }
+  },
+
+  //Material icons font does not work in production when optimize fonts option is enabled
+  optimizeFonts: false,
 }
 
 module.exports = nextConfig
