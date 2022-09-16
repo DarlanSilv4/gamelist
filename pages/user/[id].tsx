6411431ecc87d0e6a4dd0840f427b6c7624605c2
Darlan <darlan.silvadacruz@gmail.com>
2022-09-16 12:36:24 -0300

    :recycle:(pages/user) send only user data to profile page component

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -6,10 +6,8 @@ import { database } from "@firebase/firebaseConfig";
 
 import ProfilePage from "@templates/Profile";
 
-import getGames from "@lib/getGames";
-
-function Profile({ user, userGames }: { user: User; userGames: Game[] }) {
-  return <ProfilePage user={user} games={userGames} />;
+function Profile({ user }: { user: User }) {
+  return <ProfilePage user={user} />;
 }
 
 export default Profile;
@@ -28,40 +26,18 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { id } }: StaticProps) {
+  const TEEN_MINUTES = 2 * 60; //in seconds
+
   const snapshot = await get(child(ref(database), `users/${id}`));
 
   if (!snapshot.exists()) return { notFound: true };
 
   const user = snapshot.toJSON() as User;
 
-  //Firebase does not accept arrays, so it covert array in object.
-  //This code covert the object back to array
-  const userGamelist = user.gamelist
-    ? Object.values(user.gamelist).map((value: ListedGame) => {
-        return { game_id: value.game_id, state: value.state };
-      })
-    : [];
-
-  const userGamesIds: string[] = [];
-
-  if (userGamelist) {
-    const gamesIds = Object.values(userGamelist).map((value: ListedGame) => {
-      return value.game_id;
-    });
-
-    userGamesIds.push(...gamesIds);
-  }
-
-  const userGamesIdsConcatened = userGamesIds.join();
-  const query = `fields name, platforms.name, platforms.abbreviation, cover.image_id; sort name asc; limit 50; where id = (${userGamesIdsConcatened});`;
-  const userGames = await getGames(query);
-
-  const userWithGamelistInArrayFormat = { ...user, gamelist: userGamelist };
-
   return {
     props: {
-      user: userWithGamelistInArrayFormat,
-      userGames,
+      user: user,
     },
+    revalidate: TEEN_MINUTES,
   };
 }
