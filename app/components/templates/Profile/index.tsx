ab91eb34ac5741329ef3e2d1a9f163866c41f73a
Darlan <darlan.silvadacruz@gmail.com>
2022-09-16 12:44:36 -0300

    :sparkles:(templates/profile) show games according to the nav bar filter

--- app/components/templates/Profile/index.tsx
+++ app/components/templates/Profile/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Image from "next/image";
 import { useRouter } from "next/router";
+
+import { child, get, ref } from "firebase/database";
+
+import { database } from "@firebase/firebaseConfig";
 
 import Head from "@elements/Head";
 import GameCard from "@elements/GameCard";
 
 import Default from "@layouts/Default";
+
+import { createLocalAxiosInstance } from "@lib/axiosConfig";
 
 import {
   Avatar,
@@ -23,7 +29,9 @@ interface Query {
   filter?: string;
 }
 
-function ProfilePage({ user, games }: { user: User; games: Game[] }) {
+function ProfilePage({ user }: { user: User }) {
+  const ALL_GAMES = "all games";
+
   const NAV_OPTIONS: NavOptions[] = [
     "all games",
     "playing",
@@ -32,17 +40,86 @@ function ProfilePage({ user, games }: { user: User; games: Game[] }) {
     "wishlist",
   ];
 
+  const [games, setGames] = useState<Game[]>([]);
+  const [gamelist, setGamelist] = useState<ListedGame[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
 
   const { query }: { query: Query } = router;
-  const filter = query.filter?.toLowerCase();
-
-  const gamelist =
-    user.gamelist && user.gamelist instanceof Array ? user.gamelist : [];
+  const filter = query.filter?.toLowerCase() || ALL_GAMES;
 
   const isActive = (option: NavOptions) => {
     if (option === filter) return true;
-    if (filter == null && option === "all games") return true;
+    if (filter == null && option === ALL_GAMES) return true;
+  };
+
+  useEffect(() => {
+    const getGamelist = async () => {
+      const snapshot = await get(
+        child(ref(database), `users/${user.id}/gamelist`)
+      );
+
+      if (!snapshot.exists()) return;
+
+      const gamelistObj = snapshot.toJSON() as ArrayLike<ListedGame>;
+      const gamelistArray = Object.values(gamelistObj);
+
+      setGamelist(gamelistArray);
+    };
+
+    getGamelist();
+  }, []);
+
+  useEffect(() => {
+    const isValidFilter = (filter: string) => {
+      return String(NAV_OPTIONS).includes(filter);
+    };
+
+    const getGames = async () => {
+      setIsLoading(true);
+
+      if (!isValidFilter(filter)) {
+        setGames([]);
+        setIsLoading(false);
+        return;
+      }
+
+      const filteredGamelist = gamelist.filter((game) => {
+        if (game.state === filter || filter === ALL_GAMES) return game.game_id;
+      });
+
+      if (filteredGamelist.length === 0) {
+        setGames([]);
+        setIsLoading(false);
+        return;
+      }
+
+      const gamesIDs = filteredGamelist.map((game) => game.game_id);
+
+      const localAxios = createLocalAxiosInstance();
+      const { data: games } = await localAxios.post<Game[]>("/igdb/games", {
+        gamesIDs,
+      });
+
+      setGames(games);
+      setIsLoading(false);
+      return;
+    };
+
+    getGames();
+  }, [filter, gamelist]);
+
+  const displayGameCards = () => {
+    if (games.length > 0) {
+      return games.map((game, id) => {
+        return (
+          <GameCard game={game} gamelist={gamelist} key={id} isGamelistMode />
+        );
+      });
+    }
+
+    return <p>is Empty</p>;
   };
 
   return (
@@ -74,18 +151,7 @@ function ProfilePage({ user, games }: { user: User; games: Game[] }) {
           })}
         </ProfileNav>
 
-        <Games>
-          {games.map((game, id) => {
-            return (
-              <GameCard
-                game={game}
-                gamelist={gamelist}
-                key={id}
-                isGamelistMode
-              />
-            );
-          })}
-        </Games>
+        <Games>{isLoading ? <p>Loading...</p> : displayGameCards()}</Games>
       </Default>
     </React.Fragment>
   );
