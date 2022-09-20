7da66823529f356691951bc86cf5e512df5d978c
Darlan <darlan.silvadacruz@gmail.com>
2022-09-20 10:14:15 -0300

    :sparkles:(_app.tsx) display a loading bar at the top of pages when it's rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,33 @@
+import { useEffect } from "react";
+
 import type { AppProps } from "next/app";
+import Router from "next/router";
 
 import AuthProvider from "@contexts/AuthContext";
+
+import NProgress from "nprogress";
+
 import GlobalStyle from "globalStyles";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.configure({ showSpinner: false });
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <GlobalStyle />
