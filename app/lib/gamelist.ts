063e0cc2d0084dcb4533f9c902b475ff75734053
Darlan <darlan.silvadacruz@gmail.com>
2022-09-28 11:50:56 -0300

    :recycle:(gamelist.ts) remove getGamelist function

--- app/lib/gamelist.ts
+++ app/lib/gamelist.ts
@@ -1,29 +1,8 @@
 import { database } from "@firebase/firebaseConfig";
-import { ref, set, onValue } from "firebase/database";
-
-function getGamelist(
-  userId: string,
-  setGamelist: React.Dispatch<React.SetStateAction<ListedGame[]>>
-) {
-  const gamelistRef = ref(database, `users/${userId}/gamelist`);
-
-  onValue(gamelistRef, (snapshot) => {
-    const data: ListedGame | {} = snapshot.val() ?? {};
-
-    const gamelist = Object.values(data).map((value: ListedGame) => {
-      return {
-        game_id: value.game_id,
-        state: value.state,
-      };
-    });
-
-    setGamelist(gamelist);
-  });
-}
+import { ref, set } from "firebase/database";
 
 async function writeGameInTheList(game: ListedGame, userId: string) {
   const gamelistRef = ref(database, `users/${userId}/gamelist/${game.game_id}`);
   await set(gamelistRef, game);
 }
-
-export { getGamelist, writeGameInTheList };
+export { writeGameInTheList };
