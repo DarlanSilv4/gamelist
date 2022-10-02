c8fce4d6ddf71c4330feca7ea1899410e3b3e481
Darlan <darlan.silvadacruz@gmail.com>
2022-10-02 10:22:07 -0300

    :sparkles:(interfaces.d.ts) add username property in the user interface

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -38,6 +38,7 @@ interface Token {
 interface User {
   id: string;
   name: string;
+  username?: string;
   profile_summary?: string;
   avatar: string | null;
   gamelist?: ArrayLike<ListedGame>;
