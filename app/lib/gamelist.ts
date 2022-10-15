52c5559ae221e13834303ac70a88071de96c803c
Darlan <darlan.silvadacruz@gmail.com>
2022-10-15 11:32:48 -0300

    :sparkles:(gamelist.ts) create function to remove game from list

--- app/lib/gamelist.ts
+++ app/lib/gamelist.ts
@@ -5,4 +5,10 @@ async function writeGameInTheList(game: ListedGame, userId: string) {
   const gamelistRef = ref(database, `users/${userId}/gamelist/${game.game_id}`);
   await set(gamelistRef, game);
 }
-export { writeGameInTheList };
+
+async function RemoveGameFromTheList(gameId: string, userId: string) {
+  const gamelistRef = ref(database, `users/${userId}/gamelist/${gameId}`);
+  await set(gamelistRef, null);
+}
+
+export { writeGameInTheList, RemoveGameFromTheList };
