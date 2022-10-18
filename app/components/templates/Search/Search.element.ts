f495823915a6c1f772bdd55960a17d8252a03b10
Darlan <darlan.silvadacruz@gmail.com>
2022-10-18 11:45:04 -0300

    :lipstick:(templates/Search) improve responsive design

--- app/components/templates/Search/Search.element.ts
+++ app/components/templates/Search/Search.element.ts
@@ -1,17 +1,35 @@
 import styled from "styled-components";
 
 export const GamesContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  row-gap: 16px;
-  margin: 1rem 0;
-  width: 100%;
-  position: relative;
+  display: grid;
+  gap: 2rem 1rem;
+  overflow-x: hidden;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
 
   & > * {
-    height: 275px;
-    width: 205px;
+    height: 35vw;
+    width: calc(100% - 0.1rem);
+  }
+
+  @media (min-width: 520px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    row-gap: 16px;
+    margin: 1rem 0;
+    width: 100%;
+    position: relative;
+
+    & > * {
+      height: 275px;
+      width: 205px;
+    }
   }
 `;
