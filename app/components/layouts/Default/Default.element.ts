3adb0d7fd065b10eaf7ae7b063035c9464e2abe2
Darlan <darlan.silvadacruz@gmail.com>
2022-10-19 11:42:00 -0300

    :sparkles:(layouts/Deafault) add footer

--- app/components/layouts/Default/Default.element.ts
+++ app/components/layouts/Default/Default.element.ts
@@ -8,4 +8,53 @@ export const Main = styled.main`
     margin: auto;
     width: 90%;
   }
-`
+`;
+
+export const Footer = styled.footer`
+  align-items: center;
+  background-color: var(--foreground);
+  color: hsl(0, 0%, 70%);
+  display: flex;
+  flex-direction: column-reverse;
+  gap: 5px;
+  height: 56px;
+  justify-content: center;
+  margin-top: 2rem;
+  padding: 0 5%;
+  width: 90%;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+export const DevelopInfo = styled.div`
+  display: flex;
+  gap: 5px;
+
+  & span > a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+export const Github = styled.a`
+  display: none;
+
+  @media (min-width: 1024px) {
+    border-radius: 100%;
+    cursor: pointer;
+    display: initial;
+
+    &:hover {
+      background-color: var(--background);
+    }
+  }
+`;
+
+export const GithubIcon = styled.figure`
+  height: 28px;
+  position: relative;
+  width: 28px;
+`;
--- app/components/layouts/Default/index.tsx
+++ app/components/layouts/Default/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+
+import Image from "next/image";
+
 import Header from "@modules/Header";
-import { Main } from "./Default.element";
+
+import {
+  DevelopInfo,
+  Footer,
+  Github,
+  GithubIcon,
+  Main,
+} from "./Default.element";
 
 interface Props {
   children: React.ReactNode;
@@ -11,6 +21,32 @@ function Default({ children }: Props) {
     <React.Fragment>
       <Header />
       <Main>{children}</Main>
+      <Footer>
+        <DevelopInfo>
+          <span>
+            Developed by{" "}
+            <a target="_blank" href="https://github.com/DarlanSilv4">
+              Darlan Silva
+            </a>
+          </span>
+          <span>•</span>
+          <span>
+            Powered by{" "}
+            <a target="_blank" href="https://www.igdb.com/">
+              IGDB
+            </a>
+          </span>
+        </DevelopInfo>
+        <Github
+          href="https://github.com/DarlanSilv4/gamelist"
+          title="GitHub Page"
+          target="_blank"
+        >
+          <GithubIcon>
+            <Image src="/svg/github_logo.svg" layout="fill" />
+          </GithubIcon>
+        </Github>
+      </Footer>
     </React.Fragment>
   );
 }
