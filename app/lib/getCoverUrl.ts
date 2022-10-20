83790d6d201ec2bebc7933e546764495b841b8c8
Darlan <darlan.silvadacruz@gmail.com>
2022-10-20 10:43:41 -0300

    :recycle: update the image used when the game cover is missing

--- app/lib/getCoverUrl.ts
+++ app/lib/getCoverUrl.ts
@@ -2,7 +2,7 @@ function getCoverUrl(cover: Cover | undefined) {
   if (cover) {
     return `https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${cover.image_id}.jpg`;
   }
-  return "https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png";
+  return "/img/cover_missing.png";
 }
 
 export default getCoverUrl;
